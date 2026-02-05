e8d07c166b3a2865ffdd07432443fd3ac57981d5
Pranay LonarX <pranaykahalkar.lx@gmail.com>
2026-02-05 16:31:35 +0530

    Push a file

---
+++ sw.js
@@ -0,0 +1,34 @@
+// Tier-3 PWA Service Worker
+const CACHE_NAME = 'tier3-pwa-v1';
+const urlsToCache = ['./index.html','./styles.css','./script.js','./about.html','./services.html','./booking.html','./login.html','./locations.html'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) return response;
+      return fetch(event.request).then(response => {
+        if (!response || response.status !== 200 || response.type !== 'basic') return response;
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => { cache.put(event.request, clone); });
+        return response;
+      });
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
+  );
+});
